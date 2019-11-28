b833ad40ba7b107631e67a3ea74cacd59bf7d86e
YM <thehousewhite@gmail.com>
2019-11-28 23:04:13 +0100

    Appending

--- 11. Appending elements/index.js
+++ 11. Appending elements/index.js
@@ -1,17 +1,7 @@
-const a = document.querySelector('div');
-const b = d3.select("div");
-
-
-console.log("first select:");
-console.log(a);
-
-console.log("second select:");
-console.log(b);
-
-
-const selectAll = document.querySelectorAll("div");
-const selectAllD3 = d3.select("div");
-
-console.log(selectAll,selectAllD3);
-
 const canvas = d3.select(".canvas");
+const svg = canvas.append("svg");
+
+// append elements in the svg container
+svg.append("rect");
+svg.append("circle");
+svg.append("line");
