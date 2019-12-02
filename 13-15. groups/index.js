b34dd742da9e00e89442ab8168b1b08d7f5b7a50
YM <thehousewhite@gmail.com>
2019-12-02 23:12:11 +0100

    added comment for transform

--- 13-15. groups/index.js
+++ 13-15. groups/index.js
@@ -1,34 +1,38 @@
 const canvas = d3.select(".canvas");
-const svg = canvas.append("svg")
-    .attr("height", 600)
-    .attr("width", 600);
+const svg = canvas
+  .append("svg")
+  .attr("height", 600)
+  .attr("width", 600);
 
-const group = svg.append("g")
-    .attr("transform","translate(50,50)");
+const group = svg.append("g").attr("transform", `translate(50,50)`); // top and left
 
 // append elements in the svg container
-group.append("rect")
-    .attr("x",300)
-    .attr("y",100)
-    .attr("fill", "blue")
-    .attr("width",100)
-    .attr("height",200);
+group
+  .append("rect")
+  .attr("x", 300)
+  .attr("y", 100)
+  .attr("fill", "blue")
+  .attr("width", 100)
+  .attr("height", 200);
 
-group.append("circle")
-    .attr("cx",200)
-    .attr("cy",200)
-    .attr("r",50)
-    .attr("fill","pink");
-group.append("line")
-    .attr("x1",100)
-    .attr("y1", 100)
-    .attr("x2",120)
-    .attr("y2",300)
-    .attr("stroke","grey");
+group
+  .append("circle")
+  .attr("cx", 200)
+  .attr("cy", 200)
+  .attr("r", 50)
+  .attr("fill", "pink");
+group
+  .append("line")
+  .attr("x1", 100)
+  .attr("y1", 100)
+  .attr("x2", 120)
+  .attr("y2", 300)
+  .attr("stroke", "grey");
 
-svg.append("text")
-    .attr("x",20)
-    .attr("y",200)
-    .attr("fill","blue")
-    .text("Hello SVG")
-    .style("font-family","Helvetica");
+svg
+  .append("text")
+  .attr("x", 20)
+  .attr("y", 200)
+  .attr("fill", "blue")
+  .text("Hello SVG")
+  .style("font-family", "Helvetica");
